4c46adff7830de79e1ccdbc3f6df5b325dadc549
zaynlannister <zaynlannister@gmail.com>
2023-11-19 17:08:03 +0500

    feat: add tik tok download

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const tiktokDl = require("@sasmeee/tkdl");
 const { Bot } = require("grammy");
 require("dotenv").config();
 
 const bot = new Bot(process.env.BOT_TOKEN);
 
-// Reply to any message with "Hi there!".
-bot.on("message", (ctx) => ctx.reply("Hi there!"));
+bot.on("message:text", async (ctx) => {
+  const url = ctx.message.text;
+
+  await ctx.replyWithChatAction("upload_video");
+  const dataList = await tiktokDl(url);
+
+  await ctx.replyWithVideo(dataList[0].sd);
+});
 
 bot.start();
